05bd02a0903f7b91ec44bfc5e8fb2b9ef0ee1bfd
Derek Feichtinger <derek.feichtinger@psi.ch>
2007-12-12 16:03:01 +0000

    - added a script to disable/enable lists of pools

---
+++ dc_set_pools_disabled.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+usage() {
+    cat <<EOF
+Synopsis:
+          set_pools_disbled [-n] listfile
+Description:
+          The listfile must contain a list of poolnames.
+          If you specify the -n flag, all pools will be set to enabled
+EOF
+}
+
+option="disable"
+if test x"$1" = x-n; then
+    option="enable"
+    shift
+fi
+
+listfile=$1
+
+source $DCACHE_SHELLUTILS/dc_utils_lib.sh
+
+if test x"$listfile" = x; then
+    usage
+    echo "Error: no listfile  given" >&2
+    exit 1
+fi
+if test ! -r $listfile; then
+    echo "Error: Cannot read list file: $listfile" >&2
+    exit 1
+fi
+
+cmdfile=`mktemp /tmp/rep_rm-$USER.XXXXXXXX`
+if test $? -ne 0; then
+    echo "Error: Could not create a cmdfile" >&2
+    exit 1
+fi
+
+echo "cd PoolManager" >>$cmdfile
+for n in `cat $listfile`;do
+    echo "cd $n" >>$cmdfile
+    echo "pool $option" >>$cmdfile
+    echo ".." >>$cmdfile
+done
+echo ".." >>$cmdfile
+echo "logoff" >>$cmdfile
+
+execute_cmdfile $cmdfile retfile
+rm -f $cmdfile
+cat $retfile
+rm -f $retfile
