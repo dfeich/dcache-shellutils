10b25ad9e34b825d05a88096930a9bb3cdc67f4b
Derek Feichtinger <derek.feichtinger@psi.ch>
2009-08-19 10:34:39 +0000

    - command to get cell information from domains

---
+++ dc_get_routes.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+#################################################################
+# dc_get_routes.sh
+#
+# Author: Derek Feichtinger <derek.feichtinger@psi.ch> 2008-03-04
+#
+# $Id: dc_ppcopy_files.sh 1726 2009-06-25 13:12:11Z dfeich $
+#################################################################
+
+myname=$(basename $0)
+domain="dCacheDomain"
+mode="route"
+
+usage() {
+cat <<EOF
+Synopsis:
+          $myname
+
+Options:
+         -d          : cell domain name [$domain]
+         -p          : display "ps -ef" cell list instead of routes
+         -h/--help   : this help text
+
+Description:
+          By default, this command will show all the defined routes of
+          a domain (obtained by running "route" in the domain).
+          If the -p flag is specified, the output of "ps -ef" is used
+          instead
+
+EOF
+}
+
+
+##############################################################
+TEMP=`getopt -o d:hp --long help -n "$myname" -- "$@"`
+if [ $? != 0 ] ; then usage ; echo "Terminating..." >&2 ; exit 1 ; fi
+#echo "TEMP: $TEMP"
+eval set -- "$TEMP"
+
+while true; do
+    case "$1" in
+        --help|-h)
+            usage
+            exit 0
+            ;;
+       -d)
+            domain=$2
+            shift 2
+            ;;
+       -p)
+            mode="ps"
+            shift
+            ;;
+        --)
+            shift;
+            break;
+            ;;
+        *)
+            echo "Internal error!"
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+source $DCACHE_SHELLUTILS/dc_utils_lib.sh
+
+
+cmdfile=`mktemp /tmp/get_route-$USER.XXXXXXXX`
+toremove="$toremove $cmdfile"
+if test $? -ne 0; then
+    echo "Error: Could not create a cmdfile" >&2
+    rm -f $toremove
+    exit 1
+fi
+
+echo "cd /$domain" >> $cmdfile
+if test x"$mode" = xps; then
+   echo "ps -ef" >> $cmdfile
+else
+   echo "route" >> $cmdfile
+fi
+echo ".." >> $cmdfile
+echo "logoff" >> $cmdfile
+
+execute_cmdfile -f $cmdfile retfile
+toremove="$toremove $retfile"
+
+cat $retfile
+echo
+
+rm -f $toremove
