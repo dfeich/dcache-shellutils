feac6cac544f90bf695386abd7ecc7171df355a6
Derek Feichtinger <derek.feichtinger@psi.ch>
2009-11-02 09:37:58 +0000

    - only use first column of input list

--- dc_get_ID_from_pnfsnamelist.sh
+++ dc_get_ID_from_pnfsnamelist.sh
@@ -52,7 +52,7 @@ if test $? -ne 0; then
 fi
 
 echo "cd PnfsManager" >>$cmdfile
-for n in `cat $listfile`;do
+for n in `cat $listfile|cut -f1 -d' '`;do
     echo "pnfsidof $n" >>$cmdfile
 done
 echo ".." >>$cmdfile
